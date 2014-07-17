b70ed09a734c5d26cf959e4f893910c847ab27f5
Jacob <Jacob@Jacob-HP>
2014-07-17 15:50:37 -0600

    styles.css

--- codeigniter/application/views/welcome_message.php
+++ codeigniter/application/views/welcome_message.php
@@ -12,7 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url('assets/css/'); ?>/bootstrap.min.css" rel="stylesheet">
-
+    <link href="<?php echo base_url('assets/css/'); ?>/styles.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="jumbotron.css" rel="stylesheet">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
