5d352c76dc2314acd7ed9f04fabd618614b16392
Jacob <Jacob@Jacob-HP>
2014-07-23 16:26:52 -0600

    center

--- codeigniter/application/views/welcome_message.php
+++ codeigniter/application/views/welcome_message.php
@@ -29,9 +29,10 @@
   <body>
     <div class="container">
          <!--search-->
-         <h3 class="small-header">Search by:</h3>
-           <form class="center" action="dummy" method="post">
-               <select class="center" "name="choice" size="1">
+         <div class="center" 
+         <h3 Search by:</h3>
+           <form action="dummy" method="post">
+               <select name="choice" size="1">
                    <option value="">CHOOSE ONE</option>
                    <option value="locationlink">Location</option>
                    <option value="ItemCategoryLink">Item Category</option>
@@ -40,6 +41,7 @@
                </select>
                <input TYPE="button" VALUE="GO!" onClick="jump(this.form)">
            </form>
+         </div>    
      
       <!--sign in-->
       <form class="form-signin" role="form">
