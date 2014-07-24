5492b567366824f4ebefa600574a8f82c20a9d73
Jacob <Jacob@Jacob-HP>
2014-07-23 19:51:19 -0600

    practice.php

--- codeigniter/application/controllers/practice.php
+++ codeigniter/application/controllers/practice.php
@@ -1,8 +1,33 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class practice extends CI_Controller {
 
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('practice');
+	}
+
+        public function test() {
+              $this->load->database();
+              $result = $this->db->query('SELECT * FROM test');
+		var_dump($result->result());
+        }
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
