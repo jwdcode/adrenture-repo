4e04ba703e5e48768935c0207438b2a679fbf3db
Jacob <Jacob@Jacob-HP>
2014-08-04 22:00:53 -0600

    images

--- codeigniter/application/controllers/welcome.php
+++ codeigniter/application/controllers/welcome.php
@@ -2,32 +2,20 @@
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+        public function __construct() {
+        parent::__construct();
+        $this->load->model('Test');
+    }     
+
+
+    public function index()
+    {
               $this->load->view('welcome_message');
-	}
+    }
 
         public function test() {
-              $this->load->database();
-              $result = $this->db->query('SELECT * FROM test');
-		var_dump($result->result());
+              
+              $result = $this->Test->getAllTests();    
+        var_dump($result->result());
         }
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
